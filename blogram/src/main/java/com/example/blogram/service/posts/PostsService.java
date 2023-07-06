f1b68bd2af075f04fa40491959d684a616e3950f
YiHwanRyu <yihwandev@gmail.com>
2023-07-06 16:49:25 +0900

    [ADD] 수정, 삭제 API 및 화면 구현

--- blogram/src/main/java/com/example/blogram/service/posts/PostsService.java
+++ blogram/src/main/java/com/example/blogram/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.example.blogram.service.posts;
 
 import com.example.blogram.domain.posts.Posts;
 import com.example.blogram.domain.posts.repository.PostsRepository;
+import com.example.blogram.web.dto.PostsListResponseDto;
 import com.example.blogram.web.dto.PostsResponseDto;
 import com.example.blogram.web.dto.PostsSaveRequestDto;
 import com.example.blogram.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -33,4 +37,17 @@ public class PostsService {
                 () -> new IllegalArgumentException("해당 사용자가 없습니다. id= " + id));
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findALlDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+                postsRepository.delete(posts);
+    }
+
 }

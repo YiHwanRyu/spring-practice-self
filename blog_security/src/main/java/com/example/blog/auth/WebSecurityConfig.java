7f3d322b453a236c14a7c4b78074fe003dbfc0e0
YiHwanRyu <yihwandev@gmail.com>
2023-07-11 17:47:51 +0900

    [ADD] spring-security Config 파일 생성

---
+++ blog_security/src/main/java/com/example/blog/auth/WebSecurityConfig.java
@@ -0,0 +1,80 @@
+package com.example.blog.auth;
+
+import com.example.blog.jwt.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+
+@Configuration
+@EnableWebSecurity // Spring Security 지원을 가능하게 함.
+@RequiredArgsConstructor // 생성자 주입 위함.
+public class WebSecurityConfig {
+
+    private final JwtUtil jwtUtil;
+    private final UserDetailsServiceImpl userDetailsService;
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() throws Exception {
+        JwtAuthenticationFilter filter = new JwtAuthenticationFilter(jwtUtil);
+        filter.setAuthenticationManager(authenticationManager(authenticationConfiguration));
+        return filter;
+    }
+
+    @Bean
+    public JwtAuthorizationFilter jwtAuthorizationFilter() {
+        return new JwtAuthorizationFilter(jwtUtil, userDetailsService);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // CSRF 설정 해제 - Rest Api에서 stateless한 jwt토큰을 사용한다면 이미 방어되기 때문.
+        http.csrf((csrf) -> csrf.disable());
+
+        // 기본 설정인 세션방식 말고 Jwt방식을 사용하기 위한 설정(무상태)
+        http.sessionManagement((sessionManagement) ->
+                    sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                );
+
+
+        http.authorizeHttpRequests((authorizeHttpRequests) ->
+                    authorizeHttpRequests
+                            .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용
+                            .requestMatchers("/api/auth/**").permitAll() // api/auth/로 시작하는 요청 모두 접근 허가
+                            .requestMatchers(HttpMethod.GET, "/api/posts/**").permitAll() // 게시글 조회는 모두 허용
+                            .anyRequest().authenticated() // 그외 모든 요청 인증 처리
+                );
+
+        http.formLogin(Customizer.withDefaults()); // 팝업 형태의 인증절차, 기본로그인 페이지
+
+        // 필터관리
+        http.addFilterBefore(jwtAuthorizationFilter(), JwtAuthenticationFilter.class);
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}

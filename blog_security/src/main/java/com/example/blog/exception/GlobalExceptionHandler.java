6d3fa1e4af37af1cdd0ff8a2ff7f658c7a1fd471
YiHwanRyu <yihwandev@gmail.com>
2023-07-11 17:48:21 +0900

    [UPDATE] 예외처리 수정

--- blog_security/src/main/java/com/example/blog/exception/GlobalExceptionHandler.java
+++ blog_security/src/main/java/com/example/blog/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.example.blog.exception;
 import com.example.blog.dto.MessageResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -20,14 +21,14 @@ public class GlobalExceptionHandler {
         );
     }
 
-//    @ExceptionHandler({RuntimeException.class})
-//    public ResponseEntity<MessageResponseDto> handleApiException(RuntimeException ex){
-//        MessageResponseDto restApiException = new MessageResponseDto(ex.getMessage(), HttpStatus.BAD_REQUEST.toString());
-//        return new ResponseEntity<>(
-//                // HTTP body
-//                restApiException,
-//                // HTTP status code
-//                HttpStatus.BAD_REQUEST
-//        );
-//    }
+    @ExceptionHandler({UsernameNotFoundException.class})
+    public ResponseEntity<MessageResponseDto> handleApiException(UsernameNotFoundException ex){
+        MessageResponseDto restApiException = new MessageResponseDto(ex.getMessage(), HttpStatus.BAD_REQUEST.toString());
+        return new ResponseEntity<>(
+                // HTTP body
+                restApiException,
+                // HTTP status code
+                HttpStatus.BAD_REQUEST
+        );
+    }
 }
